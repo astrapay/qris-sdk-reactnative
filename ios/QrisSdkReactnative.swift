ca43f02a5150e4095dde988b5a9b055183319124
ap-ahmad-nabil <ahmad.nabil@astrapay.com>
2024-07-29 15:25:18 +0700

    chore: initial commit

---
+++ ios/QrisSdkReactnative.swift
@@ -0,0 +1,8 @@
+@objc(QrisSdkReactnative)
+class QrisSdkReactnative: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
