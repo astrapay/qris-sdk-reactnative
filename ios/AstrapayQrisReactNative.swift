c42794225c2cf549241695570e8cf9850803a580
ap-ahmad-nabil <ahmad.nabil@astrapay.com>
2024-08-27 09:41:07 +0700

    build: add configuration

--- ios/AstrapayQrisReactNative.swift
+++ ios/AstrapayQrisReactNative.swift
@@ -1,7 +1,7 @@
 import AstraPayQrisSdk
 
 @objc(AstrapayQrisReactNative)
-class AstrapayQrisReactNative: NSObject {
+class AstrapayQrisReactNative: NSObject, AstraPayQrisSdk.QRProtocolSdk {
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
@@ -12,11 +12,55 @@ class AstrapayQrisReactNative: NSObject {
     func executeQris() -> Void {
         DispatchQueue.main.async {
             let currentVc = RCTPresentedViewController()!
-            let qrisVc = QrisComponent()
-            let vc = UINavigationController(rootViewController: qrisVc)
-            vc.modalPresentationStyle = .fullScreen
-            vc.modalTransitionStyle = .crossDissolve
-            currentVc.present(vc , animated: true, completion: nil)
+            
+            AstraPayQrisSdk.QRConfigurationSdk.BUILD_MODE = .uat
+            AstraPayQrisSdk.QRConfigurationSdk.SDK_TOKEN = "XTOKEN"
+            AstraPayQrisSdk.QRConfigurationSdk.otpVersion = true
+            AstraPayQrisSdk.QRConfigurationSdk.loproVersion = 21
+            AstraPayQrisSdk.QRConfigurationSdk.isSnap = true
+            AstraPayQrisSdk.QRConfigurationSdk.qrisConfirmationVersion = 1
+//            AstraPayQrisSdk.QRConfigurationSdk.topupViewController = TopUpVc()
+            AstraPayQrisSdk.QRNewRouter.sharedInstance.delegate = self
+            AstraPayQrisSdk.QRNewRouter.sharedInstance.presentQrScan(from: currentVc)
+            
+
+//            let qrisVc = QrisComponent()
+//            let vc = UINavigationController(rootViewController: qrisVc)
+//            vc.modalPresentationStyle = .fullScreen
+//            vc.modalTransitionStyle = .crossDissolve
+//            currentVc.present(vc , animated: true, completion: nil)
         }
+    }
+    
+    func onForbidden(viewControler: UIViewController) {
+        
+    }
+    
+    func onForbidden(viewController: UIViewController) {
+        
+    }
+    
+    func onComplete(viewController: UIViewController) {
+        
+    }
+    
+    func onFailed(viewController: UIViewController) {
+        
+    }
+    
+    func onShowHistory(viewController: UIViewController) {
+        
+    }
+    
+    func onProcessing(viewController: UIViewController) {
+        
+    }
+    
+    func onCancel(viewController: UIViewController) {
+        
+    }
+    
+    func onPaylaterActivate(viewController: UIViewController) {
+        
     }
 }
